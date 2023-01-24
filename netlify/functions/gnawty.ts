1ef1cf75609450c66fccf5b2a5d03cba5b23d6e0
maciek <erbelionx@gmail.com>
2023-01-24 03:47:58 +0100

    testin

---
+++ netlify/functions/gnawty.ts
@@ -0,0 +1,22 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions"
+import fs from 'fs'
+import path from 'path'
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    // const { lang } = event.queryStringParameters;
+
+    let img = fs.readFileSync(path.resolve(__dirname, './../assets/walkin.webp'), {encoding: 'base64'})
+    // let img = fs.readFileSync('./../assets/walkin.webp', {encoding: 'base64'})
+
+    return {
+        statusCode: 200,
+        body: img,
+        isBase64Encoded: true,
+        headers: {
+            'Content-Type': 'image/webp'
+        },
+    }
+}
+
+export { handler }
+
